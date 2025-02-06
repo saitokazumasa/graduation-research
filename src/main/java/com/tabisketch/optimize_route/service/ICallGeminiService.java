0341ac61cd51e1a3070f32ff079df525a42d3ebb
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-06 09:28:06 +0900

    一旦リセット

--- src/main/java/com/tabisketch/optimize_route/service/ICallGeminiService.java
+++
@@ -1,7 +0,0 @@
-package com.tabisketch.optimize_route.service;
-
-import java.io.IOException;
-
-public interface ICallGeminiService {
-    String execute(final String systemInstruction, final String prompt) throws IOException;
-}
--- src/test/resources/sql/InsertExampleUser.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users (id, email, password, email_verified)
-VALUES (99, 'example99@example.com', '$2a$10$FFbAunp0hfeWTCune.XqwO/P/61fqWlbruV/8wqzrhM3Pw0VuXxpa', true);

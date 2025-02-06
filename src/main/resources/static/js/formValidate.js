1575eb34f2ff5fe020661a9f8f8ee197d616ee17
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2025-02-06 18:19:09 +0900

    fieldIdsにtime追加・modalTypeをそのまま使うように変更

--- src/main/resources/static/js/formValidate.js
+++ src/main/resources/static/js/formValidate.js
@@ -3,17 +3,17 @@ class FormValidator {
 
     constructor() {
         this.#fieldIds = {
-            start: ['startPlace', 'startPlaceId', 'startLat', 'startLng'],
+            start: ['startPlace', 'startPlaceId', 'startLat', 'startLng', 'startTime'],
             end: ['endPlace', 'endPlaceId', 'endLat', 'endLng'],
-            place: ['place', 'placeId', 'placeLat', 'placeLng'],
-            updateStart: ['startUpdatePlace', 'startUpdatePlaceId', 'startUpdateLat', 'startUpdateLng'],
+            places: ['place', 'placeId', 'placeLat', 'placeLng'],
+            updateStart: ['startUpdatePlace', 'startUpdatePlaceId', 'startUpdateLat', 'startUpdateLng', 'startUpdateTime'],
             updateEnd: ['endUpdatePlace', 'endUpdateLat', 'endUpdateLng'],
-            updatePlace: ['updatePlace', 'placeUpdatePlaceId', 'placeUpdateLat', 'placeUpdateLng']
+            updatePlaces: ['updatePlace', 'placeUpdatePlaceId', 'placeUpdateLat', 'placeUpdateLng']
         };
     }
 
     #isFilled(fields) {
-        return fields.every(id => document.getElementById(`${id}`).value);
+        return fields.every(id => document.getElementById(id).value);
     }
 
     #getFieldIds(prefix, num=null) {
@@ -23,54 +23,11 @@ class FormValidator {
         return this.#fieldIds[prefix];
     }
 
-    #getFieldIdsWithTime(prefix) {
-        return [...this.#getFieldIds(prefix), `${prefix}Time`];
+    #formCheck(prefix, num=null) {
+        return this.#isFilled(this.#getFieldIds(prefix, num));
     }
 
-    #formCheck(prefix, hasTime = false, num = null) {
-        return this.#isFilled(hasTime ? this.#getFieldIdsWithTime(prefix) : this.#getFieldIds(prefix, num));
-    }
-
-    #startFormCheck() {
-        return this.#formCheck('start', true);
-    }
-
-    #endFormCheck() {
-        return this.#formCheck('end');
-    }
-
-    #placeFormCheck(num) {
-        return this.#formCheck('place', false, num);
-    }
-
-    #updateStartFormCheck() {
-        return this.#formCheck('startUpdate',true);
-    }
-
-    #updateEndFormCheck() {
-        return this.#formCheck('endUpdate');
-    }
-
-    #updatePlaceFormCheck(num) {
-        return this.#formCheck('updatePlace', false, num);
-    }
-
-    validate(modalType, num) {
-        switch (modalType) {
-        case ModalType.start:
-            return this.#startFormCheck();
-        case ModalType.end:
-            return this.#endFormCheck();
-        case ModalType.places:
-            return this.#placeFormCheck(num);
-        case ModalType.updateStart:
-            return this.#updateStartFormCheck();
-        case ModalType.updateEnd:
-            return this.#updateEndFormCheck();
-        case ModalType.updatePlaces:
-            return this.#updatePlaceFormCheck(num);
-        default:
-            return false;
-        }
+    validate(modalType, num=null) {
+        return this.#formCheck(modalType, num);
     }
 }

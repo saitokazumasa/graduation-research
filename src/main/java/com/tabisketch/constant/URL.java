2fece1594ea7f4c69aede5c4a26a92c3e852f8b9
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-10 12:41:07 +0900

    URLの今ストクラスを作成

---
+++ src/main/java/com/tabisketch/constant/URL.java
@@ -0,0 +1,9 @@
+package com.tabisketch.constant;
+
+public class URL {
+    private URL() {
+    }
+
+    /// たびすけっちのURL
+    public static final String TabisketchDotCom = "https://tabisketch.com";
+}

d9a507f2932128f777e5c7beafac59a8cf9bd30a
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-10 18:01:47 +0900

    変更漏れ

--- .github/workflows/bin/init_db.sh
+++ .github/workflows/bin/init_db.sh
@@ -13,7 +13,7 @@ set +o allexport
 psql -f docker/postgresql/sql/01-create-users-table.sql
 psql -f docker/postgresql/sql/02-create-email-verification-tokens-table.sql
 psql -f docker/postgresql/sql/03-create-new-email-verification-tokens-table.sql
-psql -f docker/postgresql/sql/04-create-password-reset-tokens-table.sql
+psql -f docker/postgresql/sql/04-create-reset-password-tokens-table.sql
 psql -f docker/postgresql/sql/05-create-travel-plans-table.sql
 psql -f docker/postgresql/sql/06-create-destination-lists-table.sql
 psql -f docker/postgresql/sql/07-create-start-places-table.sql

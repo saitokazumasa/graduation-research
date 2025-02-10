beb2ddcf88082471e461527227ec6b38390d2439
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-10 12:01:56 +0900

    テスト実行時に出る余計なワーニングを解消

--- src/test/java/com/tabisketch/TabisketchApplicationTests.java
+++ src/test/java/com/tabisketch/TabisketchApplicationTests.java
@@ -1,13 +1,7 @@
 package com.tabisketch;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TabisketchApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }

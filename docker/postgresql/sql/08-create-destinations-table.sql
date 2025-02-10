f66e1aa8ade6509742d5e5db09c8f644ba990fec
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-10 12:40:24 +0900

    初期SQLをリセット

--- docker/postgresql/sql/08-create-destinations-table.sql
+++ docker/postgresql/sql/08-create-destinations-table.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS destinations (
-    id                    SERIAL           PRIMARY KEY,
-    place_id              TEXT             NOT NULL,
-    latitude              DOUBLE PRECISION NOT NULL,
-    longitude             DOUBLE PRECISION NOT NULL,
-    visit_order           INT              NOT NULL,
-    specified_start_time  TIME,
-    start_time            TIME,
-    duration_minutes      INT              NOT NULL,
-    budget                INT              NOT NULL,
-    destination_list_id   INT              NOT NULL,
-    FOREIGN KEY (destination_list_id) REFERENCES destination_lists (id)
-);
+-- CREATE TABLE IF NOT EXISTS destinations (
+--     id                    SERIAL           PRIMARY KEY,
+--     place_id              TEXT             NOT NULL,
+--     latitude              DOUBLE PRECISION NOT NULL,
+--     longitude             DOUBLE PRECISION NOT NULL,
+--     visit_order           INT              NOT NULL,
+--     specified_start_time  TIME,
+--     start_time            TIME,
+--     duration_minutes      INT              NOT NULL,
+--     budget                INT              NOT NULL,
+--     destination_list_id   INT              NOT NULL,
+--     FOREIGN KEY (destination_list_id) REFERENCES destination_lists (id)
+-- );

c9fdb1c662a472acf82c2d4ef31095d830cc7140
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2025-02-12 12:09:00 +0900

    徒歩オプションのチェック状態でdisable切り替え

--- src/main/resources/static/js/check.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const walkFlagCheckbox = document.getElementById('walkFlag');
-    const walkingTimeInput = document.getElementById('walkingTime');
-    if (walkFlagCheckbox.checked) {
-        walkingTimeInput.disabled = false;
-    }
-});

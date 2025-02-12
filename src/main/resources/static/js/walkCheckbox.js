feb734a13385a0a7f521fb62e05b9e9a257b4d55
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2025-02-12 09:59:23 +0900

    jsファイル削除、fragmentに直で記述

--- src/main/resources/static/js/walkCheckbox.js
+++
@@ -1,6 +0,0 @@
-const walkCheckbox = document.getElementById('walkFlag');
-const walkThreshold = document.getElementById('walkThreshold');
-
-walkCheckbox.addEventListener('change', () => {
-    walkThreshold.disabled = !walkCheckbox.checked;
-});

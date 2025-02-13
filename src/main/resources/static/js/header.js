5ff7062e5ae89a05aeb9fb8b8d33c9d66f7183cb
ishiki11 <m.ishiki.sys21@morijyobi.ac.jp>
2025-02-13 22:24:12 +0900

    メニュー表示時にスクロール不可、背景に色表示

--- src/main/resources/static/js/header.js
+++ src/main/resources/static/js/header.js
@@ -1,13 +1,18 @@
 const tham = document.querySelector('.tham');
 const mobileMenu = document.getElementById('mobile-menu');
+const overlay = document.getElementById('overlay');
 
 tham.addEventListener('click', () => {
-    tham.classList.toggle('tham-active');
-    mobileMenu.classList.toggle('hidden');
+    tham.classList.toggle('tham-active'); // メニューを表示
+    mobileMenu.classList.toggle('hidden'); // ハンバーガーアイコンを非表示
+    document.body.classList.toggle('overflow-hidden'); // bodyのスクロールを不可に
+    overlay.classList.toggle('hidden'); // 背景の要素の色を非表示
 
     // ウィンドウのリサイズイベント
     window.addEventListener('resize', () => {
         mobileMenu.classList.add('hidden');  // メニューを非表示にする
         tham.classList.remove('tham-active'); // ハンバーガーアイコンを元の状態に戻す
+        document.body.classList.toggle('overflow-hidden'); // bodyのスクロールを可能に戻す
+        overlay.classList.toggle('hidden'); // 背景の要素に色を表示
     });
 });

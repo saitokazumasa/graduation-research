bd0bac2404633bf9117e246d8544425e28c2e8bc
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-13 11:08:15 +0900

    CreateWaypointListRestControllerを作成（空）

---
+++ src/main/java/com/tabisketch/rest_controller/CreateWaypointListRestController.java
@@ -0,0 +1,9 @@
+package com.tabisketch.rest_controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/waypoint-list/create")
+public class CreateWaypointListRestController {
+}

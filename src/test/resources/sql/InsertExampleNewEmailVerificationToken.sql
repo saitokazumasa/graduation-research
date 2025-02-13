d35857007201e3c42122f84096ddf48e134afd0e
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-14 02:22:39 +0900

    テスト結果の精度を上げるため、テストSQLをリファクタ

--- src/test/resources/sql/InsertExampleNewEmailVerificationToken.sql
+++ src/test/resources/sql/InsertExampleNewEmailVerificationToken.sql
@@ -1,2 +1,6 @@
 INSERT INTO new_email_verification_tokens (uuid, new_email, user_id)
 VALUES ('bd725533-53a3-4a2d-9289-7fcbc7250d82', 'example2@example.com', 1);
+
+INSERT INTO new_email_verification_tokens (uuid, new_email, user_id)
+VALUES ('b7252484-8862-42a8-a14f-783b0809a1a2', 'example3@example.com', 1);
+
--- src/test/resources/sql/InsertExampleUser.sql
+++ src/test/resources/sql/InsertExampleUser.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (id, email, password, email_verified)
 VALUES (1, 'example@example.com', '$2a$10$FFbAunp0hfeWTCune.XqwO/P/61fqWlbruV/8wqzrhM3Pw0VuXxpa', TRUE);
+
+INSERT INTO users (id, email, password, email_verified)
+VALUES (2, 'example2@example.com', '$2a$10$FFbAunp0hfeWTCune.XqwO/P/61fqWlbruV/8wqzrhM3Pw0VuXxpa', TRUE);

783488df4b001d1a778258e4c537727b6f41d0e9
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-14 12:38:19 +0900

    TransporationをRouteAPIに合わせた

--- src/main/java/com/tabisketch/constant/Transporation.java
+++ src/main/java/com/tabisketch/constant/Transporation.java
@@ -2,10 +2,14 @@ package com.tabisketch.constant;
 
 /// 移動手段
 public enum Transporation {
+    /// 乗用車
+    DRIVE,
+    /// 自転車
+    BICYCLE,
     /// 徒歩
     WALK,
-    /// 車
-    DRIVING,
+    /// 二輪車
+    TWO_WHEELER,
     /// 公共交通機関
-    TRANSIT,
+    TRANSIT
 }

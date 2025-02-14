f10d3c3568f82e162e317a8750b9abb57d1c7d93
Bruin <shuu.38629@gmail.com>
2025-02-14 20:44:58 +0900

    再送信用のControllerをRegisterControllerに追加

--- src/main/java/com/tabisketch/controller/RegisterController.java
+++ src/main/java/com/tabisketch/controller/RegisterController.java
@@ -2,6 +2,7 @@ package com.tabisketch.controller;
 
 import com.tabisketch.bean.form.RegisterForm;
 import com.tabisketch.service.IRegisterService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,8 +10,10 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
@@ -49,5 +52,26 @@ public class RegisterController {
     public String send(final Model model) {
         if (!model.containsAttribute("email")) return "register/index";
         return "register/send";
+    }
+
+    @PostMapping("/send")
+    public String postSend(
+            @RequestParam("email") String email,
+            RedirectAttributes redirectAttributes
+    ) {
+        if (email == null || email.isBlank()) {
+            return "register/index";
+        }
+
+        try {
+            this.registerService.resendEmail(email);
+            redirectAttributes.addFlashAttribute("message", "メールを再送信しました。");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            redirectAttributes.addFlashAttribute("errorMessage", "メール送信に失敗しました");
+        }
+
+        redirectAttributes.addFlashAttribute("email", email);
+        return "redirect:/register/send";
     }
 }

3aceb80bf541dbd6556951af416099ec8d242979
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-14 18:24:38 +0900

    各種テーブルをfix

--- docker/postgresql/sql/08-create-departure-points-table.sql
+++ docker/postgresql/sql/08-create-departure-points-table.sql
@@ -1,7 +1,10 @@
 CREATE TABLE IF NOT EXISTS departure_points (
-    id                  SERIAL           PRIMARY KEY,
-    place_id            TEXT             NOT NULL,
-    departure_datetime  TIMESTAMP        NOT NULL,
-    waypoint_list_id    INT              NOT NULL,
+    id                  SERIAL         PRIMARY KEY,
+    label               VARCHAR(64)    NOT NULL,
+    place_id            TEXT           NOT NULL,
+    departure_datetime  TIMESTAMP      NOT NULL,
+    transportation      TRANSPORATIONS NOT NULL,
+    duration            INT            NOT NULL,
+    waypoint_list_id    INT            NOT NULL,
     FOREIGN KEY (waypoint_list_id) REFERENCES waypoint_lists (id)
 );
--- src/main/java/com/tabisketch/bean/entity/DeparturePoint.java
+++ src/main/java/com/tabisketch/bean/entity/DeparturePoint.java
@@ -1,5 +1,6 @@
 package com.tabisketch.bean.entity;
 
+import com.tabisketch.constant.Transporation;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,10 +14,16 @@ import java.time.LocalDateTime;
 public class DeparturePoint {
     /// 識別子
     private int id;
+    /// 表示名
+    private String label;
     /// Google Place Id
     private String placeId;
     /// 出発日時
     private LocalDateTime departureDatetime;
+    /// 次の地点までの移動手段
+    private Transporation transporation;
+    /// 次の地点までにかかる移動時間（秒）
+    private int duration;
     /// 関連する「行先リスト」の識別子
     private int waypointListId;
 }

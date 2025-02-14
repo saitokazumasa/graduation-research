4fa0c67eb087fcc1599dc36531e54b3004c09e28
saitokazumasa <saitokazumasa.jp@gmail.com>
2025-02-14 18:48:44 +0900

    地点周りの各種作成formを作成

---
+++ src/main/java/com/tabisketch/bean/form/CreateDestinationPointForm.java
@@ -0,0 +1,17 @@
+package com.tabisketch.bean.form;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CreateDestinationPointForm {
+    @NotBlank
+    private String label;
+
+    @NotBlank
+    private String placeId;
+}
--- src/main/java/com/tabisketch/bean/form/CreateWaypointListForm.java
+++ src/main/java/com/tabisketch/bean/form/CreateWaypointListForm.java
@@ -1,11 +1,14 @@
 package com.tabisketch.bean.form;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
@@ -17,4 +20,16 @@ public class CreateWaypointListForm {
 
     @NotNull
     private UUID planUUID;
+
+    @Valid
+    @NotNull
+    private CreateDeparturePointForm departurePoint;
+
+    @Valid
+    @NotEmpty
+    private List<CreateWaypointForm> waypointList;
+
+    @Valid
+    @NotNull
+    private CreateDestinationPointForm destinationPoint;
 }
